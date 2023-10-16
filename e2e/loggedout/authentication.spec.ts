0c81c264d32238eb3c36e98aa7d6f7ff4a2f7745
chrisshaddad <chriss.hadad@gmail.com>
2023-10-17 00:53:03 +0300

    feat: add empty test file

---
+++ e2e/loggedout/authentication.spec.ts
@@ -0,0 +1,7 @@
+test.describe("authentication flows", () => {
+  test.beforeEach(async ({ page }) => {});
+
+  test("login successfully", async ({ page }) => {});
+
+  test("login with invalid credentials", async ({ page }) => {});
+});
