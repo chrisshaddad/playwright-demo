099cec9a2d42e8dcfeb94f0b352ccc3ba5a3aee9
chrisshaddad <chriss.hadad@gmail.com>
2023-10-17 13:01:41 +0300

    feat: add more tests

--- e2e/loggedout/authentication.spec.ts
+++ e2e/loggedout/authentication.spec.ts
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.describe("authentication flows", () => {
   test.beforeEach(async ({ page }) => {
@@ -6,11 +6,23 @@ test.describe("authentication flows", () => {
     await page.goto("http://localhost:3000");
   });
 
-  test("login successfully", async () => {
-    // Test a successful login
+  test("login successfully", async ({ page }) => {
+    // Fill user details and sign in
+    await page.getByLabel("Email Address *").fill("admin");
+    await page.getByLabel("Password *").fill("admin");
+    await page.getByRole("button", { name: "Sign In" }).click();
+
+    // Make sure the user was logged in successfully
+    await expect(page).toHaveURL(/.*dashboard.*/);
   });
 
-  test("login with invalid credentials", async () => {
-    // Test a login with invalid credentials
+  test("login with invalid credentials", async ({ page }) => {
+    // Fill user details and sign in
+    await page.getByLabel("Email Address *").fill("test");
+    await page.getByLabel("Password *").fill("test");
+    await page.getByRole("button", { name: "Sign In" }).click();
+
+    // Verify we get an error message
+    expect(page.getByText("Invalid Email or Password")).toBeTruthy();
   });
 });
